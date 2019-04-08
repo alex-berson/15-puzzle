4234236d0d1d1e92cf75380ac72850d2cd63644e
Alexander Berson <aberson@yahoo.com>
2019-04-08 15:03:23 +0200

    moves

--- js/fifteen.js
+++ js/fifteen.js
@@ -24,12 +24,49 @@ const setBoard = () => {
     randomizeBoard();
     for (i = 0; i < board.length; i++){
         if (document.querySelectorAll(`#tile${i+1}`)[1]) {
+            document.querySelectorAll(`#tile${i+1}`)[1].addEventListener("click", function() {clicked(this.id)});
             document.querySelectorAll(`#tile${i+1}`)[1].innerHTML = board[i];
             document.querySelectorAll(`#tile${i+1}`)[1].id = `tile${board[i]}`;
         } else {
+            document.querySelector(`#tile${i+1}`).addEventListener("click", function() {clicked(this.id)});
             document.querySelector(`#tile${i+1}`).innerHTML = board[i];
             document.querySelector(`#tile${i+1}`).id = `tile${board[i]}`;
         }
     }
+    document.querySelector('#tile16').addEventListener("click", function() {clicked(this.id)});
+
 }
 
+const getTilesPosition = (id) => {
+    let clickedTilePosition, emptyTilePosition;
+    let tiles = document.querySelectorAll('.tile');
+    tiles.forEach(function (tile, index) {
+        if (tile.id == id) clickedTilePosition = index;
+        if (tile.id == "tile16") emptyTilePosition = index;
+    });
+        return [clickedTilePosition, emptyTilePosition];
+}
+
+const clicked = (id) => {
+    [clickedTilePosition, emptyTilePosition] = getTilesPosition(id);
+    if (isMovePossible(clickedTilePosition, emptyTilePosition)) moveTile(id, clickedTilePosition, emptyTilePosition);
+
+    console.log(clickedTilePosition, emptyTilePosition);
+}
+
+const isMovePossible = (clickedTilePosition, emptyTilePosition) => {
+    if ((Math.abs(clickedTilePosition - emptyTilePosition) == 1) || (Math.abs(clickedTilePosition - emptyTilePosition) == 4)) return true;
+    return false;
+}
+
+const moveTile = (id, clickedTilePosition, emptyTilePosition) => {
+    [board[clickedTilePosition], board[emptyTilePosition]] = [board[emptyTilePosition], board[clickedTilePosition]];
+    let clickedTileElement = document.querySelector(`#${id}`);
+    let emptyTileElement = document.querySelector('#tile16');
+    clickedTileElement.innerHTML = '';
+    clickedTileElement.id = 'tile16';
+    emptyTileElement.innerHTML = id.replace(/[^0-9]/g,'');;
+    emptyTileElement.id = id;
+}
+
+window.onload = setBoard;
