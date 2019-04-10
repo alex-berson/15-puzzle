2e0225604b1e2e8eaa3ed1ce8b5c411c900370a4
Alexander Berson <aberson@yahoo.com>
2019-04-10 12:56:08 +0200

    sliding

--- js/fifteen.js
+++ js/fifteen.js
@@ -1,12 +1,10 @@
 let board = Array.from({length: 15}, (v, i) => i+1);
 
-const isSolvable = () => {
+const isPuzzleSolvable = () => {
     let numberOfInversions = 0;
     for (i = 0; i < board.length-1; i++){
         for (j = i+1; j < board.length; j++){
-            if (board[i] > board[j]) {               
-                numberOfInversions += 1;
-            }
+            if (board[i] > board[j]) numberOfInversions++;
         }
     }
     if (numberOfInversions % 2 == 0) return true;    
@@ -14,59 +12,81 @@ const isSolvable = () => {
 }
 
 const randomizeBoard = () => {
-    do {
+    // for (let i = board.length - 1; i > 0; i--) {
+    //     const j = Math.floor(Math.random() * (i + 1));
+    //     [board[i], board[j]] = [board[j], board[i]];
+    // }
         board = board.map((a) => [Math.random(),a]).sort((a,b) => a[0]-b[0]).map((a) => a[1]);
         // board = board.sort(() => Math.random() - 0.5);
-    } while(!isSolvable());
+    if (!isPuzzleSolvable()) [board[13], board[14]] = [board[14], board[13]];
+    board.push(16);
+
 }
 
-const setBoard = () => {
+const initializeBoard = () => {
     randomizeBoard();
     for (i = 0; i < board.length; i++){
-        if (document.querySelectorAll(`#tile${i+1}`)[1]) {
-            document.querySelectorAll(`#tile${i+1}`)[1].addEventListener("click", function() {clicked(this.id)});
-            document.querySelectorAll(`#tile${i+1}`)[1].innerHTML = board[i];
-            document.querySelectorAll(`#tile${i+1}`)[1].id = `tile${board[i]}`;
-        } else {
-            document.querySelector(`#tile${i+1}`).addEventListener("click", function() {clicked(this.id)});
-            document.querySelector(`#tile${i+1}`).innerHTML = board[i];
-            document.querySelector(`#tile${i+1}`).id = `tile${board[i]}`;
-        }
+        let tile = document.querySelectorAll(`#tile${i+1}`)[1] || document.querySelector(`#tile${i+1}`);
+        tile.addEventListener("click", function() {moveTile(this.id)});
+        tile.innerHTML = board[i];
+        tile.id = `tile${board[i]}`;
     }
-    document.querySelector('#tile16').addEventListener("click", function() {clicked(this.id)});
-
-}
-
-const getTilesPosition = (id) => {
-    let clickedTilePosition, emptyTilePosition;
-    let tiles = document.querySelectorAll('.tile');
-    tiles.forEach(function (tile, index) {
-        if (tile.id == id) clickedTilePosition = index;
-        if (tile.id == "tile16") emptyTilePosition = index;
-    });
-        return [clickedTilePosition, emptyTilePosition];
-}
-
-const clicked = (id) => {
-    [clickedTilePosition, emptyTilePosition] = getTilesPosition(id);
-    if (isMovePossible(clickedTilePosition, emptyTilePosition)) moveTile(id, clickedTilePosition, emptyTilePosition);
-
-    console.log(clickedTilePosition, emptyTilePosition);
 }
 
 const isMovePossible = (clickedTilePosition, emptyTilePosition) => {
-    if ((Math.abs(clickedTilePosition - emptyTilePosition) == 1) || (Math.abs(clickedTilePosition - emptyTilePosition) == 4)) return true;
+    if ((Math.abs(clickedTilePosition - emptyTilePosition) == 1) ||
+        (Math.abs(clickedTilePosition - emptyTilePosition) == 4)) return true;
     return false;
 }
 
-const moveTile = (id, clickedTilePosition, emptyTilePosition) => {
-    [board[clickedTilePosition], board[emptyTilePosition]] = [board[emptyTilePosition], board[clickedTilePosition]];
+const moveTile = id => {
+
+    let clickedTilePosition = board.indexOf(parseInt(id.replace(/[^0-9]/g,'')));
+    let emptyTilePosition = board.indexOf(16);
+
+    if (!isMovePossible(clickedTilePosition, emptyTilePosition)) return;
+
     let clickedTileElement = document.querySelector(`#${id}`);
     let emptyTileElement = document.querySelector('#tile16');
-    clickedTileElement.innerHTML = '';
-    clickedTileElement.id = 'tile16';
-    emptyTileElement.innerHTML = id.replace(/[^0-9]/g,'');;
-    emptyTileElement.id = id;
+
+    const offset = Math.abs(clickedTileElement.offsetLeft - emptyTileElement.offsetLeft ||
+        clickedTileElement.offsetTop - emptyTileElement.offsetTop);
+
+    let position = 0;
+
+    let  moveInterval = setInterval(sliding, 5);
+    function sliding() {
+        if ((position <= -offset) || (position >= offset)) {     
+            clearInterval(moveInterval);
+            clickedTileElement.innerHTML = '';
+            clickedTileElement.id = 'tile16';
+            clickedTileElement.style.removeProperty('left');
+            clickedTileElement.style.removeProperty('top');
+            clickedTileElement.style.removeProperty('right');
+            clickedTileElement.style.removeProperty('bottom');
+
+            emptyTileElement.innerHTML = id.replace(/[^0-9]/g,'');
+            emptyTileElement.id = id;
+            [board[clickedTilePosition], board[emptyTilePosition]] = [board[emptyTilePosition], board[clickedTilePosition]];
+        } else {
+            position += 4;
+            switch(emptyTilePosition - clickedTilePosition) {
+            case 1:   
+                direction = 'left'; 
+                break;
+            case -1: 
+                direction = 'right'; 
+                break;
+            case 4:   
+                direction = 'top';
+                break;
+            case -4:   
+                direction = 'bottom'; 
+                break;
+            } 
+        clickedTileElement.style.setProperty(direction, position + 'px');
+        }
+    }
 }
 
-window.onload = setBoard;
+window.onload = initializeBoard;
